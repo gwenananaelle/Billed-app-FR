0cfbca63c10fbf3c280d0d16deadfb869194cef2
gwenaelle <gwenaelle.duchesne@gmail.com>
2021-12-03 15:59:57 +0100

    cleaning

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,7 +5,7 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { formatDate, formatStatus } from "../app/format.js"
+import { formatDate } from "../app/format.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
